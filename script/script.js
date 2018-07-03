2a5a7097db41a9f248bf09a319a93de93f5c1535
kento-oka <kento-oka@kentoka.com>
2018-07-03 12:15:07 +0900

    githubページはリポジトリ名ごとにディレクトリを切っている

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 (function(){
     var ALERT_MSG   = {
-        "generate_pw": "パスワードを初期化しました。\n新パスワードは AbcDef12$ です。"
+        "generate_pw": "パスワードを初期化しました。\n新パスワードは AbcDef12$ です。",
+        "change_pw": "パスワードを変更しました。"
     };
     var alert_list      = document.getElementsByClassName("js_alert")
         alert_list_len  = alert_list.length,
