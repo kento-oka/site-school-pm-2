a760665b239c5e41146e1a8badef77ebe49c4a7c
kento-oka <kento-oka@kentoka.com>
2018-07-03 12:05:28 +0900

    権限ごとにページ分けするの開始

---
+++ script/script.js
@@ -0,0 +1,15 @@
+(function(){
+    var ALERT_MSG   = {
+        "generate_pw": "パスワードを初期化しました。\n新パスワードは AbcDef12$ です。"
+    };
+    var alert_list      = document.getElementsByClassName("js_alert")
+        alert_list_len  = alert_list.length,
+        i
+    ;
+
+    for(i = 0; i < alert_list_len; i++){
+        alert_list[i].addEventListener("click", function(){
+            alert(ALERT_MSG[alert_list[i].getAttribute("data-alert")])
+        });
+    }
+})();
